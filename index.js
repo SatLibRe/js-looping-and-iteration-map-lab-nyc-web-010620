140dee5695855bc8ba4d2f960d5ba331a1922600
Miles Higbie <miles.higbie@gmail.com>
2020-02-17 16:10:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers){
+  return drivers.map(function (el){
+    return el.toLowerCase()
+  })
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    const fName = driver.split(' ')[0];
+    const lName = driver.split(' ')[1];
+
+    return { firstName: fName, lastName: lName };
+  });
+}
+
+function attributesToPhrase(drivers){
+  return drivers.map(function(el){
+    return `${el.name} is from ${el.hometown}`
+  })
+}
