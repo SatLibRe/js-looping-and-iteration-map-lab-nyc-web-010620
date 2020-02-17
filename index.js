832055064d7a5a636d70922bd251006561153462
Miles Higbie <miles.higbie@gmail.com>
2020-02-17 16:01:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function lowerCaseDrivers(drivers){
 }
 
 function nameToAttributes(drivers){
+   const splitNameDrivers = drivers.map(function(el){
+   return el.split(" ")
+ }) 
+
+const fNameDrivers = splitNameDrivers.map(function(el){
+  return el[0]
+})
+
+const lNameDrivers = splitNameDrivers.map(function(el){
+  return el[1]
+})
+
   return drivers.map(function (el){
     return Object.assign({}, drivers, {firstName: drivers})
   })
